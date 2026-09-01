45c1eefd15625c8f402b0407fa6b66881480e74c
Yibeltal Yitayew <sewmehoneyibeltal540@gmail.com>
2026-09-01 21:39:49 +0300

    Define user profile data model and initial dataset

---
+++ js/data/userData.js
@@ -0,0 +1,24 @@
+/**
+ * Core profile statistics mock data source
+ */
+
+export const userProfile = {
+  id: "usr_948201",
+  username: "metages_dev",
+  name: "Metages Yibeltal",
+  role: "Full-Stack Software Engineer",
+  avatar:
+    "https://images.unsplash.com/photo-1534528741775-53994a69daeb?auto=format&fit=crop&w=250&q=80",
+  joinedDate: "2023-01-15",
+  stats: {
+    totalViews: 142850,
+    profileVisits: 28410,
+    contributions: 1482,
+    reputationScore: 98.4,
+  },
+  socials: {
+    github: "github.com/Yetemare-Yibeltal",
+    linkedin: "linkedin.com/in/metages",
+    twitter: "@metages_dev",
+  },
+};
